4eeb6455262fb8946f1d958f97a9f00a208c3ed4
nkaskov <mail.nikita.kaskov@gmail.com>
2024-03-07 18:00:15 +0000

    CMake configuration added.

---
+++ cpp/bin/signum/README.md
@@ -0,0 +1,3 @@
+# Sign Evaluation Challenge
+This binary was developed in a result of the Fherma [Challenge](https://fherma.io/challenges/652bf668485c878710fd020a) dedicated to Sign Function Evaluation.
+
---
+++ cpp/src/signum/ckks.cpp
@@ -0,0 +1,70 @@
+#include "signum/ckks.hpp"
+
+SignumCKKS::SignumCKKS(std::string ccLocation, std::string pubKeyLocation, std::string multKeyLocation,
+                       std::string rotKeyLocation,
+                       std::string inputLocation,
+                       std::string outputLocation)
+    : m_PubKeyLocation(pubKeyLocation),
+      m_MultKeyLocation(multKeyLocation),
+      m_RotKeyLocation(rotKeyLocation),
+      m_CCLocation(ccLocation),
+      m_InputLocation(inputLocation),
+      m_OutputLocation(outputLocation)
+{
+    initCC();
+};
+
+void SignumCKKS::initCC()
+{
+    if (!Serial::DeserializeFromFile(m_CCLocation, m_cc, SerType::JSON))
+    {
+        std::cerr << "Could not deserialize cryptocontext file" << std::endl;
+        std::exit(1);
+    }
+
+    if (!Serial::DeserializeFromFile(m_PubKeyLocation, m_PublicKey, SerType::BINARY))
+    {
+        std::cerr << "Could not deserialize public key file" << std::endl;
+        std::exit(1);
+    }
+
+    std::ifstream multKeyIStream(m_MultKeyLocation, std::ios::in | std::ios::binary);
+    if (!multKeyIStream.is_open())
+    {
+        std::exit(1);
+    }
+    if (!m_cc->DeserializeEvalMultKey(multKeyIStream, SerType::BINARY))
+    {
+        std::cerr << "Could not deserialize rot key file" << std::endl;
+        std::exit(1);
+    }
+
+    std::ifstream rotKeyIStream(m_RotKeyLocation, std::ios::in | std::ios::binary);
+    if (!rotKeyIStream.is_open())
+    {
+        std::exit(1);
+    }
+
+    if (!m_cc->DeserializeEvalAutomorphismKey(rotKeyIStream, SerType::BINARY))
+    {
+        std::cerr << "Could not deserialize eval rot key file" << std::endl;
+        std::exit(1);
+    }
+
+    if (!Serial::DeserializeFromFile(m_InputLocation, m_InputC, SerType::BINARY))
+    {
+        std::exit(1);
+    }
+}
+
+void SignumCKKS::eval()
+{
+}
+
+void SignumCKKS::deserializeOutput()
+{
+    if (!Serial::SerializeToFile(m_OutputLocation, m_OutputC, SerType::BINARY))
+    {
+        std::cerr << " Error writing ciphertext 1" << std::endl;
+    }
+}
