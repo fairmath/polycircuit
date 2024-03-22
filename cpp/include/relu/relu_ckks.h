9222fec1067bd40e9e18d139385142a5a2fc2ff5
Alexandra Mirzuitova <alexandra.mirzuitova@gmail.com>
2024-03-22 11:19:29 +0200

    Placeholders for new challenges

---
+++ cpp/include/relu/relu_ckks.h
@@ -0,0 +1,35 @@
+#include "openfhe.h"
+
+// header files needed for serialization
+#include "ciphertext-ser.h"
+#include "cryptocontext-ser.h"
+#include "key/key-ser.h"
+#include "scheme/ckksrns/ckksrns-ser.h"
+
+using namespace lbcrypto;
+
+class ReluCKKS
+{
+    CryptoContext<DCRTPoly> m_cc;
+    PublicKey<DCRTPoly> m_PublicKey;
+    Ciphertext<DCRTPoly> m_InputC;
+    Ciphertext<DCRTPoly> m_OutputC;
+    std::string m_PubKeyLocation;
+    std::string m_MultKeyLocation;
+    std::string m_RotKeyLocation;
+    std::string m_CCLocation;
+    std::string m_InputLocation;
+    std::string m_OutputLocation;
+
+public:
+    ReluCKKS(std::string ccLocation, std::string pubKeyLocation, std::string multKeyLocation,
+               std::string rotKeyLocation,
+               std::string inputLocation,
+               std::string outputLocation);
+
+    void initCC();
+
+    void eval();
+
+    void serializeOutput();
+};
